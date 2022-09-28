1a55e9a0349b3d37ce501d3db6aff814a93e5c90
qian1170 <94057027+qian1170@users.noreply.github.com>
2022-09-28 14:30:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
 import streamlit as st
-st.write("hello")
+import matplotlib.pyplot as plt
+from bokeh.plotting import figure
+import plotly.figure_factory as ff
+import numpy as np
+import pandas as pd
+
+st.title('My streamlit first app')
+
+st.title('My app title is : Wonderfuf app')
+st.write('hello,*World!*:sunglasses: ')
+
+st.write('st.write accept text, but also other data formats, such as numbers,data frames, styled data frames, and sassorted objects,... ')
+st.write(1,2,3,4)
+st.write(pd.DataFrame(
+    {'first column':[1,2,3,4],
+    'second column':[10,20,30,40]
+    }
+))
+
+data_frame=pd.DataFrame(np.random.randn(200,3),columns=['a','b','c'])
+st.write('st.write accepts also multiple arguments like ')
+st.write('1=1=',2)
+st.write('below is DataFrame:',data_frame,'Above is a dataframe')
+st.title('Displaying data elements with code snippets:')
